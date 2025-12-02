1b1ba1ce6fea2461b0dcb78962f37e2fd41c2f21
devgio <giovanaganzer.deleon@gmail.com>
2025-12-02 19:58:49 -0300

    funcoes nao testadas

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 import json
+import os
+
+perguntas = "questions.json"
+
+
+def load_questions():
+    if os.path.exists(perguntas):
+        try:
+            with open(perguntas, "r", encoding="utf-8") as f:
+                return json.load(f)
+        except json.JSONDecodeError:
+            return []
+    return []
+
+
+def save_questions(pergunta):
+    with open(perguntas,"w",encoding="UTF-8") as f:
+        json.dump(perguntas,f,ensure_ascii=False,indent=4)
+    
